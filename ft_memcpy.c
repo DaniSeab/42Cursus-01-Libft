c14dc21010053783ee572fd20aaafc8c0b667dfe
dlima-se <dlima-se@student.42sp.org.br>
2022-09-27 23:42:49 +0200

    memcpy e strrchr alt pos moulinette

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: dlima-se <dlima-se@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/08 19:29:14 by dlima-se          #+#    #+#             */
-/*   Updated: 2022/09/26 19:37:41 by dlima-se         ###   ########.fr       */
+/*   Updated: 2022/09/27 23:20:43 by dlima-se         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,12 @@
 
 void	*ft_memcpy(void *dest, const void *src, size_t len)
 {
-	size_t	i;
+	char		*d;
+	const char	*s;
 
-	i = 0;
-	if (!dest || !src)
-		return (0);
-	while (i < len)
-	{
-		((unsigned char *)dest)[i] = ((unsigned char *)src)[i];
-		i++;
-	}
+	d = (char *)dest;
+	s = (const char *)src;
+	while (len--)
+		*d++ = *s++;
 	return (dest);
 }
--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: dlima-se <dlima-se@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/08 19:30:08 by dlima-se          #+#    #+#             */
-/*   Updated: 2022/09/20 01:02:33 by dlima-se         ###   ########.fr       */
+/*   Updated: 2022/09/27 23:36:35 by dlima-se         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,13 @@
 
 char	*ft_strrchr(const char *str, int ch)
 {
-	int			i;
 	const char	*start;
 	char		c;
 
 	c = (char)ch;
-	i = ft_strlen(str);
 	start = str;
-	str += i;
-	while (*str != *start && *str != c)
+	str += ft_strlen(str);
+	while (str != start && *str != c)
 		str--;
 	if (*str == c)
 		return ((char *)str);
