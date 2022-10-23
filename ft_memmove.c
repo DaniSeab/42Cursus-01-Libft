f6e3b9c0ee07d338eceaa2ac05fa300249766a71
Dani Seabra <103091767+DaniSeab@users.noreply.github.com>
2022-10-23 20:56:42 -0300

    OK + commented [23/10/22]

--- ft_memmove.c
+++ ft_memmove.c
@@ -12,6 +12,19 @@
 
 #include "libft.h"
 
+/* replicates the string.h function memmove.
+Function copies n bytes from memory area src to memory area dest. Is overlap safe.	
+Overlap means that they share memory area. eg: if adress of dest comes before src
+src   =    [b][c][d] 
+dest  = [a][b][c]
+result= [b][c][d]
+i can copy freely from src to dest from left to right; but if address of dest comes after
+src   = [a][b][c]
+dest  =    [b][c][d]
+result=    [a][a][a]
+when i go to copy slot [b] it has already been replaced by [a]. So i need to copy from
+back to front in this case.
+*/
 void	*ft_memmove(void *dest, const void *src, size_t len)
 {
 	size_t		i;
