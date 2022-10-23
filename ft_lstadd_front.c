362a01c2b5223224763ff44b6712bd675ebbb4c3
Dani Seabra <103091767+DaniSeab@users.noreply.github.com>
2022-10-23 14:12:31 -0300

    OK + commented [23/10/22]

--- ft_lstadd_front.c
+++ ft_lstadd_front.c
@@ -12,15 +12,20 @@
 
 #include "libft.h"
 
+//linked list function. Adds new node to beggining of list
 void	ft_lstadd_front(t_list **lst, t_list *new)
 {
+	//if new points to Null, don't add it
 	if (!new)
 		return ;
+	//if list is empty, just point lst to new node
 	if (!*lst)
 	{
 		*lst = new;
 		return ;
 	}
+	//otherwise, set new's "next" variable to adress of lst (head), which is the adress of 1st node
 	new->next = *lst;
+	//set lst(head) to hold the adress of new node
 	*lst = new;
 }
