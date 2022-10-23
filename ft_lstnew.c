b1c9c787117bbd35aa84c74f48609b7860eec60f
Dani Seabra <103091767+DaniSeab@users.noreply.github.com>
2022-10-23 14:35:02 -0300

    OK + commented [23/10/22]

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -12,14 +12,20 @@
 
 #include "libft.h"
 
+//linked list function. Creates new node.
 t_list	*ft_lstnew(void *content)
 {
+	//creates new variable * to hold a type t_list
 	t_list	*new;
 
+	//mallocates heap memory of size t_list, to contain the variables visible in .h file
 	new = (t_list *)malloc(sizeof(t_list));
+	//if malloc fails, return null *
 	if (!new)
 		return (0);
+	//sets nodes "content" variable to input content
 	new->content = content;
+	//sets nodes "next" variable to NULL
 	new->next = NULL;
 	return (new);
 }
