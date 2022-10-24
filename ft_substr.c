980239f309a226bcd557ef5f4a8446599804f237
Dani Seabra <103091767+DaniSeab@users.noreply.github.com>
2022-10-23 21:31:19 -0300

    OK + commented [23/10/22]

--- ft_substr.c
+++ ft_substr.c
@@ -12,18 +12,22 @@
 
 #include "libft.h"
 
+//creates a string based off s starting at index start with len size.
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*res;
 	size_t	i;
 
 	i = 0;
+	//if theres no index start in s, return 1 mallocated NULL byte
 	if (start >= ft_strlen(s))
 	{
 		res = ft_calloc(1, 1);
 		return (res);
 	}
+	//to avoid trailing off the string, test if desired new string fits inside s
 	if ((len + (size_t)start) > ft_strlen(s))
+		//if it does not, set len to the max size it can have
 		len = ft_strlen(s) - (size_t)start;
 	res = ft_calloc(len + 1, sizeof(char));
 	if (!res)
